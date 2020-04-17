13179a3012fee9dc1220b451fc6f5c7070ff81df
SailentzZz <mishatravin99@gmail.com>
2020-04-17 20:53:06 +0300

    first commit

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+
+    public AudioSource SoundEff;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		SoundEff.Play();
+	}
+}
